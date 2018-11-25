bc763a1dc6be19f1fd4341413f985347a8beac58
Elias Dolinsek <elias.dolinsek@gmail.com>
2018-11-25 15:36:42 +0100

    Started implementing new CockpitStatisticFragment

--- app/src/main/java/com/dolinsek/elias/cashcockpit/MainActivity.java
+++ app/src/main/java/com/dolinsek/elias/cashcockpit/MainActivity.java
@@ -1,35 +1,19 @@
 package com.dolinsek.elias.cashcockpit;
 
-import android.preference.PreferenceFragment;
-import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import 	android.support.v4.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
-import com.dolinsek.elias.cashcockpit.components.Bill;
 import com.dolinsek.elias.cashcockpit.components.Database;
-import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 import org.json.JSONException;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private BottomNavigationView mBottomNavigationView;
 
-    private CockpitFragment cockpitFragment = new CockpitFragment();
+    private CockpitStatisticsFragment cockpitStatisticsFragment = new CockpitStatisticsFragment();
     private HistoryFragment historyFragment = new HistoryFragment();
     private StatisticsFragment statisticsFragment = new StatisticsFragment();
     private DatabaseFragment databaseFragment = new DatabaseFragment();
@@ -55,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
         if(savedInstanceState == null){
             //Show CockpitFragment
-            replaceFragment(cockpitFragment);
+            replaceFragment(cockpitStatisticsFragment);
         }
 
         mBottomNavigationView = (BottomNavigationView) findViewById(R.id.bnv_main);
@@ -67,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
                     replaceFragment(databaseFragment);
                         return true;
                 case R.id.navigation_cockpit:
-                    replaceFragment(cockpitFragment);
+                    replaceFragment(cockpitStatisticsFragment);
                         return true;
                 case R.id.navigation_history:
                     replaceFragment(historyFragment);
